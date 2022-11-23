386c5f6664eaddc8757b61f8e52abcfe70ca144e
Steven E. Collins <iamstevenecollins@gmail.com>
2022-11-23 09:45:10 -0500

    Initial Upload

---
+++ SwiftUIStack/SwiftUIStack/SwiftUIStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStackApp.swift
+//  SwiftUIStack
+//
+//  Created by stevenecollins on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
